6fffa8b8351bf7c5fcd8cbf807fc8008c5aa03ca
Santia545 <corc180903@gmail.com>
2023-03-24 12:58:56 -0600

    solved missing R class in metronomeactivity class error

--- app/src/main/java/com/example/guitartraina/activities/metronome/MetronomeActivity.java
+++ app/src/main/java/com/example/guitartraina/activities/metronome/MetronomeActivity.java
@@ -3,6 +3,8 @@ package com.example.guitartraina.activities.metronome;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.guitartraina.R;
+
 public class MetronomeActivity extends AppCompatActivity {
 
     @Override
