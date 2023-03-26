92bb76ef87e138d3fbe316719c0a5bffa14128ee
Santia545 <corc180903@gmail.com>
2023-03-26 00:04:46 -0600

    intento de iframe jajaja

--- app/src/main/java/com/example/guitartraina/activities/LooperActivity.java
+++ app/src/main/java/com/example/guitartraina/activities/LooperActivity.java
@@ -29,15 +29,13 @@ public class LooperActivity extends AppCompatActivity {
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
-        // Enable JavaScript interface to access video player state and current time
-        webView.addJavascriptInterface(new JavaScriptInterface(), "Android");
 
         String videoId = "Sb5aq5HcS1A";
         String videoUrl = "https://www.youtube.com/embed/" + videoId + "?enablejsapi=1";
 
         // Set the WebView layout params to match the video player size
         int videoWidth = 640;
-        int videoHeight = 360;
+        int videoHeight = 390;
         float videoAspectRatio = (float) videoWidth / videoHeight;
         int webViewWidth = getResources().getDisplayMetrics().widthPixels;
         int webViewHeight = Math.round(webViewWidth / videoAspectRatio);
@@ -46,42 +44,18 @@ public class LooperActivity extends AppCompatActivity {
         layoutParams.width = webViewWidth;
         layoutParams.height = webViewHeight;
         webView.setLayoutParams(layoutParams);
-
-        webView.loadUrl(videoUrl);
+        playVideo(videoId);
     }
-
+    private void playVideo(String videoId) {
+        String iframe = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/oH7IHDVzE7Q?enablejsapi=1&origin=https%3A%2F%2Fwww.looper.tube&widgetid=1\" frameborder=\"0\" allowfullscreen=\"1\"></iframe>";
+        webView.loadData(iframe, "text/html", "utf-8");
+    }
     @Override
     public void onBackPressed() {
         if (webView.canGoBack()) {
             webView.goBack();
         } else {
             super.onBackPressed();
-        }
-    }
-
-    // JavaScript interface to access video player state and current time
-    private class JavaScriptInterface {
-
-        @JavascriptInterface
-        public void onPlayerStateChange(int state) {
-            // Handle video player state change
-            switch (state) {
-                case 0: // Video ended
-                    Toast.makeText(LooperActivity.this, "Video ended", Toast.LENGTH_SHORT).show();
-                    break;
-                case 1: // Video playing
-                    Toast.makeText(LooperActivity.this, "Video playing", Toast.LENGTH_SHORT).show();
-                    break;
-                case 2: // Video paused
-                    Toast.makeText(LooperActivity.this, "Video paused", Toast.LENGTH_SHORT).show();
-                    break;
-            }
-        }
-
-        @JavascriptInterface
-        public void onCurrentTimeChange(float currentTime) {
-            // Handle current time change
-            Toast.makeText(LooperActivity.this, "Current time: " + currentTime, Toast.LENGTH_SHORT).show();
         }
     }
 }
