40c38eb24a2e552eddd1d303e694762aeda562f7
Santia545 <corc180903@gmail.com>
2023-06-22 00:44:47 -0600

    it records but in a slower playback rate, the problem is not on the mic is on the writer processor, cause i listen to the audio in real time and it sounds great

--- app/src/main/java/com/example/guitartraina/activities/ranking/RecorderActivity.java
+++ app/src/main/java/com/example/guitartraina/activities/ranking/RecorderActivity.java
@@ -1,6 +1,7 @@
 package com.example.guitartraina.activities.ranking;
 
 import static android.media.AudioFormat.CHANNEL_IN_MONO;
+import static android.media.AudioFormat.CHANNEL_OUT_MONO;
 import static android.media.AudioFormat.ENCODING_PCM_16BIT;
 
 import androidx.activity.result.ActivityResultLauncher;
@@ -10,14 +11,13 @@ import androidx.core.app.ActivityCompat;
 import androidx.preference.PreferenceManager;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.media.AudioRecord;
+import android.media.AudioTrack;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.Handler;
 import android.provider.Settings;
 import android.view.Gravity;
@@ -33,7 +33,9 @@ import android.widget.Toast;
 import com.example.guitartraina.R;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.Map;
 
@@ -41,6 +43,8 @@ import be.tarsos.dsp.AudioDispatcher;
 import be.tarsos.dsp.AudioProcessor;
 import be.tarsos.dsp.GainProcessor;
 import be.tarsos.dsp.io.TarsosDSPAudioFormat;
+import be.tarsos.dsp.io.UniversalAudioInputStream;
+import be.tarsos.dsp.io.android.AndroidAudioPlayer;
 import be.tarsos.dsp.io.android.AudioDispatcherFactory;
 import be.tarsos.dsp.writer.WriterProcessor;
 
@@ -50,11 +54,19 @@ public class RecorderActivity extends AppCompatActivity {
     private TextView tvSeconds, tvNoteCounter;
     private Thread recorderThread = null;
     private AudioDispatcher dispatcher = null;
+    int CHANNELS = 1;
+    int BIT_DEPTH = 16;
+    boolean BIG_ENDIAN = false;
     private final int SAMPLE_RATE = 44100;
+    TarsosDSPAudioFormat audioFormat = new TarsosDSPAudioFormat(TarsosDSPAudioFormat.Encoding.PCM_SIGNED, SAMPLE_RATE, BIT_DEPTH, CHANNELS, 2, 1, BIG_ENDIAN);
     private final int RECORD_BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL_IN_MONO, ENCODING_PCM_16BIT);
     private final int RECORD_BUFFER_OVERLAP = RECORD_BUFFER_SIZE / 2;
-    private final Runnable stopRecording = () -> {Toast.makeText(RecorderActivity.this, "Deteniendo", Toast.LENGTH_SHORT).show(); dispatcher.stop();};
-    ;
+    private final Runnable stopRecording = () -> {
+        Toast.makeText(RecorderActivity.this, "Deteniendo", Toast.LENGTH_SHORT).show();
+        dispatcher.stop();
+        togglePlayReRecordPublishButtons(true);
+    };
+
     private final Handler handler = new Handler();
 
     @Override
@@ -66,13 +78,14 @@ public class RecorderActivity extends AppCompatActivity {
         btnPlay = findViewById(R.id.play_btn);
         btnReRecord = findViewById(R.id.re_record_btn);
         btnPublish = findViewById(R.id.publish_btn);
-        disablePlayReRecordPublishButtons();
+        togglePlayReRecordPublishButtons(false);
         btnRecord.setOnClickListener(view -> {
             view.setEnabled(false);
             view.setAlpha(0.5f);
             recordAudio();
-            handler.postDelayed(stopRecording, 5000);
+            handler.postDelayed(stopRecording, 2500);
         });
+        btnPlay.setOnClickListener(view -> playSound());
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             String[] permissions = {
                     Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -94,56 +107,83 @@ public class RecorderActivity extends AppCompatActivity {
         }
     }
 
-    private void disablePlayReRecordPublishButtons() {
-        btnPlay.setEnabled(false);
-        btnReRecord.setEnabled(false);
-        btnPublish.setEnabled(false);
-        btnPlay.setAlpha(0.5f);
-        btnReRecord.setAlpha(0.5f);
-        btnPublish.setAlpha(0.5f);
-    }
-
-    private void recordAudio() {
-        int CHANNELS = 1;
-        int BIT_DEPTH = 16;
-        boolean BIG_ENDIAN = false;
-        int SAMPLE_RATE = 44100;
-        TarsosDSPAudioFormat audioFormat = new TarsosDSPAudioFormat(TarsosDSPAudioFormat.Encoding.PCM_SIGNED, SAMPLE_RATE, BIT_DEPTH, CHANNELS, 2, 1, BIG_ENDIAN);
-        double gain = getGainFromPreferences();
-        dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(SAMPLE_RATE, RECORD_BUFFER_SIZE, RECORD_BUFFER_OVERLAP);
-        RandomAccessFile randomAccessFile = null;
-        try {
-            randomAccessFile = new RandomAccessFile(RecorderActivity.this.getExternalFilesDir(null) + "/audioTest.wav", "rw");
-        } catch (FileNotFoundException notFoundException) {
-            File file = new File(RecorderActivity.this.getExternalFilesDir(null), "audioTest.wav");
-            try {
-                if (!file.exists()) {
-                    file.createNewFile();
-                    System.out.println("no existe");
-                }
-                randomAccessFile = new RandomAccessFile(file, "rw");
-            } catch (Exception ignored) {
-                ignored.printStackTrace();
-            }
+    private void togglePlayReRecordPublishButtons(boolean enabled) {
+        if (!enabled) {
+            btnPlay.setEnabled(true);
+            btnReRecord.setEnabled(false);
+            btnPublish.setEnabled(false);
+            btnPlay.setAlpha(0.5f);
+            btnReRecord.setAlpha(0.5f);
+            btnPublish.setAlpha(0.5f);
+        } else {
+            btnPlay.setEnabled(true);
+            btnReRecord.setEnabled(true);
+            btnPublish.setEnabled(true);
+            btnPlay.setAlpha(1f);
+            btnReRecord.setAlpha(1f);
+            btnPublish.setAlpha(1f);
         }
-        AudioProcessor gainProcessor = new GainProcessor(gain);
-        WriterProcessor writerProcessor = new WriterProcessor(audioFormat, randomAccessFile);
-        dispatcher.addAudioProcessor(gainProcessor);
-        dispatcher.addAudioProcessor(writerProcessor);
-        recorderThread = new Thread(dispatcher, "Audio Dispatcher");
-        recorderThread.setPriority(Thread.MAX_PRIORITY);
-        recorderThread.start();
-    }
 
+    }
     private double getGainFromPreferences() {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(RecorderActivity.this);
         int microphoneGain = sharedPreferences.getInt("microphone_gain", 10);
         return microphoneGain / 10.d;
     }
 
+    private void recordAudio() {
+        double gain = getGainFromPreferences();
+        dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(SAMPLE_RATE, RECORD_BUFFER_SIZE, RECORD_BUFFER_OVERLAP);
+        AudioProcessor gainProcessor = new GainProcessor(gain);
+        File file = new File(getExternalFilesDir(null), "audioTest");
+        if (file.exists()) {
+            file.delete(); // Delete the file if it already exists
+        }
+        RandomAccessFile randomAccessFile = null;
+        try {
+            file.createNewFile(); // Create a new file
+            randomAccessFile = new RandomAccessFile(file, "rw");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        AndroidAudioPlayer androidAudioPlayer= new AndroidAudioPlayer(audioFormat);
+        WriterProcessor writerProcessor = new WriterProcessor(audioFormat,randomAccessFile);
+        dispatcher.addAudioProcessor(gainProcessor);
+        dispatcher.addAudioProcessor(writerProcessor);
+        dispatcher.addAudioProcessor(androidAudioPlayer);
+        recorderThread = new Thread(dispatcher, "Audio Dispatcher");
+        recorderThread.setPriority(Thread.MAX_PRIORITY);
+        recorderThread.start();
+    }
+
+    private void playSound() {
+        int BUFFER_SIZE = AudioTrack.getMinBufferSize(SAMPLE_RATE,CHANNEL_OUT_MONO,ENCODING_PCM_16BIT);
+        File file = new File(getExternalFilesDir(null), "audioTest");
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            UniversalAudioInputStream audioStream = new UniversalAudioInputStream(fileInputStream, audioFormat);
+            AudioDispatcher dispatcher = new AudioDispatcher(audioStream, BUFFER_SIZE, BUFFER_SIZE/2);
+            AndroidAudioPlayer player = new AndroidAudioPlayer(audioFormat);
+            dispatcher.addAudioProcessor(player);
+            Thread audioDispatcher = new Thread(dispatcher, "Audio Dispatcher");
+            audioDispatcher.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle the IOException
+        }
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        if(recorderThread!=null){
+            try {
+                recorderThread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
         handler.removeCallbacks(stopRecording);
     }
 
