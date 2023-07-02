39c3db4fd7bb146eeac47a3c5a1844e4926d0b2d
PauPau-Cor <PauPauCoronaP@gmail.com>
2023-07-01 23:16:51 -0600

    Now shows testAds throughout the app every 10 minutes if the user hasn't paid the pro version.

--- app/src/main/java/com/example/guitartraina/activities/MainActivity.java
+++ app/src/main/java/com/example/guitartraina/activities/MainActivity.java
@@ -1,21 +1,43 @@
 package com.example.guitartraina.activities;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 import com.example.guitartraina.R;
-import com.example.guitartraina.services.GoogleAdmobService;
+import com.example.guitartraina.activities.tuner.YoutubePlayerActivity;
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.security.crypto.EncryptedSharedPreferences;
+import androidx.security.crypto.MasterKeys;
 
 import com.example.guitartraina.databinding.ActivityMainBinding;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 public class MainActivity extends AppCompatActivity {
+    private final Handler handler = new Handler();
+    private Runnable runnable;
+    private SharedPreferences archivo;
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,17 +54,115 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        startActivity(new Intent(this, YoutubePlayerActivity.class)
+                .putExtra("video", "\"https://www.youtube.com/embed/xyb74jO1QkA\"")
+                .putExtra("titulo", R.string.tutorial)
+                .putExtra("cuerpo", R.string.crear_afinacion_personalizada));
+
+        getEncryptedSharedPreferences();
+        boolean proStatus = !getUserType().equals("Pago");
+        // 10 minutes = 600000 ms
+
+        if(proStatus){
+            MobileAds.initialize(this, initializationStatus -> {
+                runnable = () -> {
+                    Log.d(TAG, "GoogleAdmobService: showing intersitial ad...");
+                    showAd();
+                    handler.postDelayed(runnable, 600000);
+                };
+                handler.postDelayed(runnable, 600000);
+            });
+        }
+    }
+
+    private void showAd() {
+        AdRequest adRequest = new AdRequest.Builder().build();
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        // The mInterstitialAd reference will be null until
+                        // an ad is loaded.
+                        mInterstitialAd = interstitialAd;
+                        setupFullScreenCallback();
+                        mInterstitialAd.show(MainActivity.this);
+                        Log.i(TAG, "onAdLoaded");
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error
+                        Log.d(TAG, loadAdError.toString());
+                        mInterstitialAd = null;
+                    }
+                });
+    }
+
+    private void setupFullScreenCallback() {
+        mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback(){
+            @Override
+            public void onAdClicked() {
+                // Called when a click is recorded for an ad.
+                Log.d(TAG, "Ad was clicked.");
+            }
+
+            @Override
+            public void onAdDismissedFullScreenContent() {
+                // Called when ad is dismissed.
+                // Set the ad reference to null so you don't show the ad a second time.
+                Log.d(TAG, "Ad dismissed fullscreen content.");
+                mInterstitialAd = null;
+            }
+
+            @Override
+            public void onAdFailedToShowFullScreenContent(@NonNull AdError adError) {
+                // Called when ad fails to show.
+                Log.e(TAG, "Ad failed to show fullscreen content.");
+                mInterstitialAd = null;
+            }
+
+            @Override
+            public void onAdImpression() {
+                // Called when an impression is recorded for an ad.
+                Log.d(TAG, "Ad recorded an impression.");
+            }
+
+            @Override
+            public void onAdShowedFullScreenContent() {
+                // Called when ad is shown.
+                Log.d(TAG, "Ad showed fullscreen content.");
+            }
+        });
+    }
+
+    private String getUserType() {
+        if (archivo.contains("idUsuario")) {
+            return archivo.getString("idUsuario", "notlogged");
+        }
+        return "0";
+    }
+
+    private void getEncryptedSharedPreferences() {
+        String masterKeyAlias;
+        archivo = null;
+        try {
+            masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC);
+            archivo = EncryptedSharedPreferences.create(
+                    "archivo",
+                    masterKeyAlias,
+                    this,
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            );
+        } catch (GeneralSecurityException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        startService(new Intent(this, GoogleAdmobService.class));
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        stopService(new Intent(this, GoogleAdmobService.class));
+    public void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(runnable);
     }
 }
